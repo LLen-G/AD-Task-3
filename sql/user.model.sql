52d4078ff97279e6bc26d5f35e969416d51c4604
LLen-G <christianallengalang@gmail.com>
2025-07-07 22:55:20 +0800

    refactor: renamed files

--- sql/user.model.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.user_info (
-    id int NOT NULL,
-    username varchar(256) NOT NULL,
-    password varchar(256) NOT NULL,
-    first_name varchar(256) NOT NULL,
-    middle_name varchar(256),
-    last_name varchar(256) NOT NULL,
-    email varchar(256) NOT NULL
-);
